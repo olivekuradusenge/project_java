4c8d97725315ab01f00dbe16fe9be0718eed3b1d
olivekuradusenge <olive1kuradusenge@gmail.com>
2025-12-14 18:01:12 +0200

    Add files via upload

---
+++ student.java
@@ -0,0 +1,31 @@
+package model;
+
+public class student {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+
+    public student() {}
+
+    public student(int id, String firstName, String lastName, String email, String phone) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    // Getters and setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone = phone; }
+}
